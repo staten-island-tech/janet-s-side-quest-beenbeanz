c45ec3ba243aa9381f1ecf5f968392d993a8575a
beenbeanz <haileylan404@gmail.com>
2025-03-07 09:26:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,36 @@ def csv_to_list(file_path):
 
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
-print(data)  # Output the list
 
-
-
-
+#1
+def avgSalesPerDay(data):
+    row_totals = {}
     
+    for row in data[1:]:
+        store_name = row[0]
+        sales = map(int, row[1:])
+        row_totals[store_name] = sum(sales) / 30
+
+    return row_totals
+
+print(avgSalesPerDay(data))
+
+
+
+
+#3
+def allStoresAvgSales(data):
+    row_totals = []
+    
+    for row in data[1:]:
+        store_name = row[0]
+        sales = map(int, row[1:])
+        row_totals.append(sum(sales))
+
+   # for i in row_totals:
+        
+    return row_totals
+
+allStoresAvgSalesList = allStoresAvgSales(data)
+print(allStoresAvgSalesList)
 
