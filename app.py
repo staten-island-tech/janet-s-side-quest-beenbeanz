ca9a9fe3b35efd21d2700305a70e09fca7701881
beenbeanz <haileylan404@gmail.com>
2025-03-11 08:39:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,35 +14,37 @@ def csv_to_list(file_path):
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
 
-#1
-def avgSalesPerDay(data):
-    row_totals = {}
-    
+#1  //every store all 30 days
+"""def avgSalesPerDay(data):    row_totals = {}
     for row in data[1:]:
         store_name = row[0]
         sales = map(int, row[1:])
-        row_totals[store_name] = sum(sales) / 30
-
+        row_totals[store_name] = round(sum(sales) / 30,2)
     return row_totals
+print(avgSalesPerDay(data))"""
 
-print(avgSalesPerDay(data))
-
-
-
-
-#3
-def allStoresAvgSales(data):
+#2
+"""def mostProfitable(data):
     row_totals = []
-    
     for row in data[1:]:
-        store_name = row[0]
         sales = map(int, row[1:])
         row_totals.append(sum(sales))
-
-   # for i in row_totals:
-        
+        row_totals.sort()
     return row_totals
+print(mostProfitable(data))"""
 
-allStoresAvgSalesList = allStoresAvgSales(data)
-print(allStoresAvgSalesList)
 
+#3 every store every day
+def allStoresAvgSales(data):
+    row_totals = []
+    for row in data[1:]:
+        sales = map(int, row[1:])
+        row_totals.append(sum(sales))
+    avg = sum(row_totals) / len(row_totals)        
+    return avg
+
+#4
+def closingStores(data):
+    twentyPercentOfAverage = allStoresAvgSales(data) * .2
+    return 
+closingStores(data)
